3f233a66236438443b15756293b263ae05f59e81
satoukatumi18 <satoukatumi18@gmail.com>
2020-08-13 11:20:23 +0900

    テクスチャの実装

--- src/main/java/com/sakastudio/dominatormod/DOMINATORmod.java
+++ src/main/java/com/sakastudio/dominatormod/DOMINATORmod.java
@@ -1,8 +1,12 @@
 package com.sakastudio.dominatormod;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
@@ -13,6 +17,8 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import com.sakastudio.dominatormod.network.PacketHandler;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(
         modid = DOMINATORmod.MOD_ID,
@@ -31,13 +37,19 @@ public class DOMINATORmod{
     //この関数でMODファイル自体をイベントの発火先にする。
     public void construct(FMLConstructionEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
-        PacketHandler.registerMessages("dominatormod");
+        PacketHandler.registerMessages(MOD_ID);
     }
 
     //アイテムを登録するイベント。旧preinitのタイミングで発火する。
     @SubscribeEvent
     public void registerItems(RegistryEvent.Register<Item> event) {
         event.getRegistry().register(DOMINATOR);
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void registerModels(ModelRegistryEvent event) {
+        ModelLoader.setCustomModelResourceLocation(DOMINATOR, 0, new ModelResourceLocation(new ResourceLocation(MOD_ID, "dominator"), "inventory"));
     }
 
     //画面内にエンティティがいる時のイベント
