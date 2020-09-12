6e019e654e6389cbb43575dc334590eab706d555
satoukatumi18 <satoukatumi18@gmail.com>
2020-09-12 17:04:26 +0900

    キルログの追加

--- src/main/java/com/sakastudio/dominatormod/network/PacketServerPlayerKill.java
+++ src/main/java/com/sakastudio/dominatormod/network/PacketServerPlayerKill.java
@@ -3,12 +3,14 @@ package com.sakastudio.dominatormod.network;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.InventoryEnderChest;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntityEnderChest;
+import net.minecraft.util.DamageSource;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
@@ -16,6 +18,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import com.sakastudio.dominatormod.Utility;
 
 import java.util.List;
+import java.util.Random;
 
 public class PacketServerPlayerKill implements IMessage {
     public int EntityID;
@@ -60,8 +63,7 @@ public class PacketServerPlayerKill implements IMessage {
             for (EntityPlayerMP item: list) {
                 //ターゲットのプレイヤーである
                 if(message.EntityID == item.getEntityId()){
-                    //そのプレイヤーをキル
-                    item.onKillCommand();
+
                     InventoryEnderChest ender = item.getInventoryEnderChest();
                     //エンダーチェストの中身をドロップ
                     for (int i = 0;i<ender.getSizeInventory();i++){
@@ -70,11 +72,27 @@ public class PacketServerPlayerKill implements IMessage {
                         ender.setInventorySlotContents(i,new ItemStack(new Block(Material.AIR)));
                     }
 
+                    //そのキルのソース
+                    DamageSource d;
 
                     if(message.isClash){
                         //相手プレイヤーをクラッシュ
                         PacketHandler.INSTANCE.sendTo(new PacketClientClash(),item);
+
+                        //ソースを設定
+                        Random random = new Random();
+                        int r = random.nextInt(6)+1;
+                        d = new DamageSource("BanSibyl"+r).setDamageAllowedInCreativeMode();
+                    }else {
+                        //ソースを設定
+                        Random random = new Random();
+                        int r = random.nextInt(5)+1;
+                        d = new DamageSource("Sibyl"+r).setDamageAllowedInCreativeMode();
                     }
+                    //キル
+                    item.attackEntityFrom(d,100000);
+
+
                     break;
                 }
             }
